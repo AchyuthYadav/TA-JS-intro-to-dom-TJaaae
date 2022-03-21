579b3065efff9ff0a13f301037a896a9b50bebaf
achyuth yadav <achyuthyadav@hmail.com>
2022-03-21 15:22:32 +0530

    assignment

--- block-BJaadz/code/script.js
+++ block-BJaadz/code/script.js
@@ -1,27 +1,45 @@
 // Select the h1 element and store it in a variable named heading.
 
+let heading = document.querySelector("h1")
+
+
 // Check the typeof heading and log it.
+
+typeof heading 
 
 // Change the color of heading to black.
 
+heading.style.color = black
+
 // Select all the hr elements and store it in a variable named allHrs using querySelectorAll
+
+let allHrs = querySelectorAll("hr")
 
 /* 
 Convert the NodeList returned by querySelectorAll to Array using Array.from() or spread operator and store it in allHrsArray
-
 Array.from converts an array kind of data into array so we can use methods like map, reduce
-
 HINT:
 let allHrsArray = Array.from(allHrs)
 */
 
+
+
+
 // Set the border of the all the hr elements to "1px solid tomato"
+
+
 
 // Change the background of all the hr to "antiquewhite" using for of loop.
 
+
+
 // Change the 'border-radius' of all the hr to "5px" using array.
 
+
+
 // Change the alignment of the heading(h1) to center.
+
+
 
 // Change the font size of the heading to 3rem.
 
@@ -33,31 +51,67 @@ let allHrsArray = Array.from(allHrs)
 
 // Create a pragraph element and store it in variable named 'para' using `createElement`
 
+let para = document.createElement("p")
+
 // Change the inner text of para to "querySelector returns an element you can maupulate but querySelectorAll returns the collection of elements in array kind of structure."
+
+para.innerText = "querySelector returns an element you can maupulate but querySelectorAll returns the collection of elements in array kind of structure."
 
 // Remove all the elements from box 1
 
+
+
 // Replace all the elements inside box 1 with the para (you created above)
+
 
 /* Walking the DOM
 Do the following after selecting box 16 and storing in variable named box16
 
   - Access the parentNode
+
+
   - Access the childNodes
+
+
   - Access previousSibling
+
+
   - Access nextSibling
+
+
   - Access firstChild
+
+
   - Access lastChild
 
+
   - Access previousElementSibling
+
+
   - Access nextElementSibling
+
+
   - Access firstElementChild
+
+
   - Access lastElementChild
+
+
 
   - Focus on the difference between element and node
 */
 
+
+let box16 = document.querySelector("box sixteen")
+
+
+
+
+
+
 // Select box 2 and append a new paragraph element with content "Append inserts as last child" just after hr element.
+
+
 
 // Select box 3 and prepend a new paragraph element with content "Prepend inserts as first child" just before hr element.
 
